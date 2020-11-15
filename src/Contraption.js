41175d58b549a54f2f652de0bff0ff01d174de05
ashen-develops <ashenp.designs@gmail.com>
2020-11-15 15:45:15 -0600

    FINALLY got the player functioning somehow

--- src/Contraption.js
+++ src/Contraption.js
@@ -1,54 +1,84 @@
 import React from 'react';
 import * as Tone from 'tone';
-import { Player } from 'tone';
+import mp3 from "./Media/Backing/2bop_back160.mp3"
 
-function Contraption() {
-    // const twoBopB = new Tone.Player('./Media/Backing/2bop_back160.mp3').toDestination();
-    // twoBopB.autostart = true;
-    // twoBopB.loop = true;
-    return(
-    <div>
-    <main>
+// const buffer = new Tone.ToneAudioBuffer("./Media/Backing/2bop_back160.mp3", () => {
+//     console.log("loaded");
+// });
+// function load(){
+//     buffer.load()
+// }
+const player = new Tone.Player({mp3}).toDestination();
+
+
+class Contraption extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            toggle:false,
+            mp3: "./Media/Backing/2bop_back160.mp3"
+          }
+    }
+    handlePlay = () => {
+        const pianoSamples = new Tone.ToneAudioBuffers({
+            A1: "https://tonejs.github.io/audio/casio/A1.mp3",
+            A2: mp3,
+        }, () => {
+            const player = new Tone.Player().toDestination();
+            // play one of the samples when they all load
+            player.buffer = pianoSamples.get("A2");
+            player.loop = true;
+            player.start();
+        });
+    }
+    
+    render(){
+        return(
         <div>
-            <h3>Start By Dragging and Dropping Some Beats</h3>
-        </div>
-        <div className="music-box">
+        <main>
+
             <div>
-                <button 
-                className="twobeat"
-                // onClick={twoBopB}
-                // on click add bop back to the loop (2bop_back160)
-                ></button>
-                <button 
-                className="twobeat"
-                // on click add bop back to the loop (2bop_back160)
-                ></button>
+                <h3>Start By Dragging and Dropping Some Beats</h3>
             </div>
-            <div>
-                <button 
-                className="onebeat"
-                // on click add bongo beat to the loop (bongo2_beat160)
-                >    
-                </button>
-                <button 
-                className="onebeat"
-                // on click add bongo beat to the loop(bongo2_beat160)
-                ></button>
-                <button 
-                className="onebeat"
-                // on click add bongo beat to the loop(bongo2_beat160)
-                ></button>
-                <button 
-                className="onebeat"
-                // on click add bongo beat to the loop(bongo2_beat160)
-                ></button>
+            <div className="music-box">
+                <div>
+                    {/* <a href={this.twoBopB()}>link</a> */}
+                    <button 
+                    className="twobeat"
+                    onClick={this.handlePlay}
+                    // on click add bop back to the loop (2bop_back160)
+                    ></button>
+                    <button 
+                    className="twobeat"
+                    // on click add bop back to the loop (2bop_back160)
+                    ></button>
+                </div>
+                <div>
+                    <button 
+                    className="onebeat"
+                    // on click add bongo beat to the loop (bongo2_beat160)
+                    >    
+                    </button>
+                    <button 
+                    className="onebeat"
+                    // on click add bongo beat to the loop(bongo2_beat160)
+                    ></button>
+                    <button 
+                    className="onebeat"
+                    // on click add bongo beat to the loop(bongo2_beat160)
+                    ></button>
+                    <button 
+                    className="onebeat"
+                    // on click add bongo beat to the loop(bongo2_beat160)
+                    ></button>
+                </div>
+
             </div>
 
+        </main>
         </div>
-
-    </main>
-    </div>
-    )
+        )
+    }
 };
 
 export default Contraption;
---
+++ src/practice.js
@@ -0,0 +1,10 @@
+// import * as Tone from 'tone';
+
+// const player = new Tone.Player({
+//     url: "https://tonejs.github.io/audio/loop/FWDL.mp3",
+//     loop: true,
+//     loopStart: 0,
+//     loopEnd: 1,
+// }).toDestination();
+
+// player.autostart = true;
